4e2af52765e4f9795b7db1910fff39063750d462
llamacoder <30612433+llamacoder@users.noreply.github.com>
2018-03-12 14:41:05 -0600

    reverted lists to demo, trying to get mobile lists to work

--- public/main.js
+++ public/main.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var docElem = document.documentElement;
-  var sortgrid = document.querySelector('.sortable-grid');
-  var board = sortgrid.querySelector('.board');
-  var itemContainers = Array.prototype.slice.call(sortgrid.querySelectorAll('.board-column-content'));
+  var itemContainers = [].slice.call(document.querySelectorAll('.board-column-content'));
   var columnGrids = [];
-  var dragCounter = 0;
   var boardGrid;
 
+  // Define the column grids so we can drag those
+  // items around.
   itemContainers.forEach(function (container) {
 
-    var muuri = new Muuri(container, {
+    // Instantiate column grid.
+    var grid = new Muuri(container, {
       items: '.board-item',
       layoutDuration: 400,
       layoutEasing: 'ease',
@@ -24,32 +23,42 @@ document.addEventListener('DOMContentLoaded', function () {
       dragReleaseEasing: 'ease'
     })
     .on('dragStart', function (item) {
-      ++dragCounter;
-      docElem.classList.add('dragging');
+      // Let's set fixed widht/height to the dragged item
+      // so that it does not stretch unwillingly when
+      // it's appended to the document body for the
+      // duration of the drag.
       item.getElement().style.width = item.getWidth() + 'px';
       item.getElement().style.height = item.getHeight() + 'px';
     })
-    .on('dragEnd', function (item) {
-      if (--dragCounter < 1) {
-        docElem.classList.remove('dragging');
-      }
-    })
     .on('dragReleaseEnd', function (item) {
+      // Let's remove the fixed width/height from the
+      // dragged item now that it is back in a grid
+      // column and can freely adjust to it's
+      // surroundings.
       item.getElement().style.width = '';
       item.getElement().style.height = '';
-      columnGrids.forEach(function (muuri) {
-        muuri.refreshItems();
+      // Just in case, let's refresh the dimensions of all items
+      // in case dragging the item caused some other items to
+      // be different size.
+      columnGrids.forEach(function (grid) {
+        grid.refreshItems();
       });
     })
     .on('layoutStart', function () {
+      // Let's keep the board grid up to date with the
+      // dimensions changes of column grids.
       boardGrid.refreshItems().layout();
     });
 
-    columnGrids.push(muuri);
+    // Add the column grid reference to the column grids
+    // array, so we can access it later on.
+    columnGrids.push(grid);
 
   });
 
-  boardGrid = new Muuri(board, {
+  // Instantiate the board grid so we can drag those
+  // columns around.
+  boardGrid = new Muuri('.board', {
     layoutDuration: 400,
     layoutEasing: 'ease',
     dragEnabled: true,
@@ -60,5 +69,4 @@ document.addEventListener('DOMContentLoaded', function () {
     dragReleaseDuration: 400,
     dragReleaseEasing: 'ease'
   });
-
 });
